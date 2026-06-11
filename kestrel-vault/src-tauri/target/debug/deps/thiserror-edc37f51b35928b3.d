74cd415696d90438954f9773c6fec7f821812ee5
Z User <z@container>
2026-06-11 05:13:36 +0000

    503b5e5e-1e22-412d-8778-a7581bb3af07

---
+++ kestrel-vault/src-tauri/target/debug/deps/thiserror-edc37f51b35928b3.d
@@ -0,0 +1,14 @@
+/home/z/my-project/kestrel-vault/src-tauri/target/debug/deps/thiserror-edc37f51b35928b3.d: /home/z/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/thiserror-2.0.18/src/lib.rs /home/z/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/thiserror-2.0.18/src/aserror.rs /home/z/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/thiserror-2.0.18/src/display.rs /home/z/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/thiserror-2.0.18/src/var.rs /home/z/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/thiserror-2.0.18/src/private.rs /home/z/my-project/kestrel-vault/src-tauri/target/debug/build/thiserror-4edeecf0dd90c32f/out/private.rs
+
+/home/z/my-project/kestrel-vault/src-tauri/target/debug/deps/libthiserror-edc37f51b35928b3.rlib: /home/z/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/thiserror-2.0.18/src/lib.rs /home/z/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/thiserror-2.0.18/src/aserror.rs /home/z/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/thiserror-2.0.18/src/display.rs /home/z/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/thiserror-2.0.18/src/var.rs /home/z/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/thiserror-2.0.18/src/private.rs /home/z/my-project/kestrel-vault/src-tauri/target/debug/build/thiserror-4edeecf0dd90c32f/out/private.rs
+
+/home/z/my-project/kestrel-vault/src-tauri/target/debug/deps/libthiserror-edc37f51b35928b3.rmeta: /home/z/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/thiserror-2.0.18/src/lib.rs /home/z/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/thiserror-2.0.18/src/aserror.rs /home/z/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/thiserror-2.0.18/src/display.rs /home/z/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/thiserror-2.0.18/src/var.rs /home/z/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/thiserror-2.0.18/src/private.rs /home/z/my-project/kestrel-vault/src-tauri/target/debug/build/thiserror-4edeecf0dd90c32f/out/private.rs
+
+/home/z/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/thiserror-2.0.18/src/lib.rs:
+/home/z/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/thiserror-2.0.18/src/aserror.rs:
+/home/z/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/thiserror-2.0.18/src/display.rs:
+/home/z/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/thiserror-2.0.18/src/var.rs:
+/home/z/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/thiserror-2.0.18/src/private.rs:
+/home/z/my-project/kestrel-vault/src-tauri/target/debug/build/thiserror-4edeecf0dd90c32f/out/private.rs:
+
+# env-dep:OUT_DIR=/home/z/my-project/kestrel-vault/src-tauri/target/debug/build/thiserror-4edeecf0dd90c32f/out
